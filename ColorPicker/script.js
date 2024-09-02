28c96526e7ec5d65ed820150a862756eadc0f81a
BHANU-CHINNAPAREDDY <bhanu33725@gmail.com>
2024-09-02 14:16:29 +0530

    Added Color Picker Project

---
+++ ColorPicker/script.js
@@ -0,0 +1,19 @@
+
+let spanElement=document.getElementById("selectedColorHexCode");
+
+function changeBgToGreyAndUpdateText(){
+    document.body.style.backgroundColor = "#e0e0e0";
+    spanElement.textContent="#e0e0e0";
+}
+function changeBgToGreenAndUpdateText(){
+    document.body.style.backgroundColor = "#6fcf97";
+    spanElement.textContent="#6fcf97";
+}
+function changeBgToBlueAndUpdateText(){
+    document.body.style.backgroundColor = "#56ccf2";
+    spanElement.textContent="#56ccf2";
+}
+function changeBgToPurpleAndUpdateText(){
+    document.body.style.backgroundColor = "#bb6bd9";
+    spanElement.textContent="#bb6bd9";
+}
